116dcc5eb93398bd4daeb6c69088e353702c7e6e
GuillermoJCV <memo92975@gmail.com>
2024-09-02 18:38:50 -0600

    Añade el cors al proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const isProduction = process.env.PRODUCTION
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //SWAGGER CONFIG
   const config = new DocumentBuilder()
   .setTitle("Leafyshop Api")
   .setDescription("La api de nuestra tienda virtual LeafyShop")
   .setVersion("1.0")
   .build();
-
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("docs", app, document)
+
+  // CORS CONFIG
+  app.enableCors({
+    origin : isProduction ? "" : "*"
+  })
+
+  // LISTEN
   await app.listen(3000);
 }
 bootstrap();
